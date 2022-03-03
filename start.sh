9950a1757f08b3f69fde1c529fd051fcd30855be
Marylin Erlich <merlich@oa-b3.msk.21-school.ru>
2022-03-03 21:14:58 +0300

    removed some unused files

--- start.sh
+++
@@ -1,2 +0,0 @@
-clang -Wall -Wextra -Werror bonus/main_bonus.c bonus/stdin_parser_bonus.c bonus/error_bonus.c bonus/close_fd_bonus.c bonus/childs_bonus.c bonus/list_functions_bonus.c -o pipex
-./pipex 1.txt "cat -e" "grep 123" "cat -e" "wc -l" 2.txt
